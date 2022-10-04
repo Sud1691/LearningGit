edb8af7effff3a6024a974f9967269455355b6b8
Sud1691 <sudhanshu1691@gmail.com>
2022-10-04 21:49:03 +0530

    updated main.tf.

--- main.tf
+++ main.tf
@@ -73,20 +73,6 @@ resource "aws_route_table" "publicRouteTableA" {
   }
 }
 
-resource "aws_route_table" "publicRouteTableB" {
-  vpc_id = aws_vpc.TrainingVPC.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-
-  tags = {
-    Name = "publicRouteTableB"
-  }
-}
-
 resource "aws_route_table_association" "PublicAssociationA" {
   subnet_id      = aws_subnet.publicSubnetA.id
   route_table_id = aws_route_table.publicRouteTableA.id
@@ -94,7 +80,7 @@ resource "aws_route_table_association" "PublicAssociationA" {
 
 resource "aws_route_table_association" "PublicAssociationB" {
   subnet_id      = aws_subnet.publicSubnetB.id
-  route_table_id = aws_route_table.publicRouteTableB.id
+  route_table_id = aws_route_table.publicRouteTableA.id
 }
 
 
