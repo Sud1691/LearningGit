7f00d4b7fa0c3e9ec5bb3d812b9d4e0a3a535c2f
Sud1691 <sudhanshu1691@gmail.com>
2022-10-13 20:30:07 +0530

    updated.

--- main.tf
+++ main.tf
@@ -5,14 +5,11 @@ terraform {
       version = "~> 4.16"
     }
   }
-
   required_version = ">= 1.2.0"
 }
-
 provider "aws" {
   region = "us-west-2"
 }
-
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
@@ -21,16 +18,12 @@ resource "aws_instance" "app_server" {
     Name = "ExampleAppServerInstance"
   }
 }
-
-
 resource "aws_eip" "TrainingEIPA" {
   vpc = true
 }
-
 resource "aws_eip" "TrainingEIPB" {
   vpc = true
 }
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.TrainingVPC.id
 
@@ -38,36 +31,27 @@ resource "aws_internet_gateway" "igw" {
     Name = "TrainingIGW"
   }
 }
-
 resource "aws_nat_gateway" "TrainingNGWA" {
   allocation_id = aws_eip.TrainingEIPA.id
   subnet_id     = aws_subnet.publicSubnetA.id
-
   tags = {
     Name = "TrainingNGWA"
   }
 
 }
-
 resource "aws_nat_gateway" "TrainingNGWB" {
   allocation_id = aws_eip.TrainingEIPB.id
   subnet_id     = aws_subnet.publicSubnetB.id
-
   tags = {
     Name = "TrainingNGWB"
   }
-
 }
-
 resource "aws_route_table" "publicRouteTableA" {
   vpc_id = aws_vpc.TrainingVPC.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-
-
   tags = {
     Name = "publicRouteTableA"
   }
